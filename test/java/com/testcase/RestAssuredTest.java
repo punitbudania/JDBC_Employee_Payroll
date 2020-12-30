6345a1732e676db358a6fdfb9e715a7b36094f17
punitbudania <punitbudania88@gmail.com>
2020-12-30 18:42:02 +0530

    [punit]Added: multiple employee details using thread

--- test/java/com/testcase/RestAssuredTest.java
+++ test/java/com/testcase/RestAssuredTest.java
@@ -1,15 +1,25 @@
 package com.testcase;
 
+import com.employeepayroll.EmployeePayrollData;
+import com.employeepayroll.EmployeePayrollService;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class RestAssuredTest
 {
@@ -45,7 +55,6 @@ public class RestAssuredTest
                             .body("{\"name\": \"Lisa\", \"salary\": \"8000\"}")
                             .when()
                             .put("/employees/3");
-        String respAsStr = response.asString();
         response.then().body("name", Matchers.is("Lisa"));
         response.then().body("salary", Matchers.is("8000"));
     }
@@ -68,5 +77,55 @@ public class RestAssuredTest
         MatcherAssert.assertThat(statusCode, CoreMatchers.is(200));
         response = RestAssured.get("/employees");
         response.then().body("id", Matchers.not(1));
+    }
+
+    @Test
+    public void givenMultipleEmployee_OnPost_ShouldReturnAddedEmployee()
+    {
+        EmployeePayrollData[] arrayOfEmps = {
+                new EmployeePayrollData(0, "Ajit", 4000.0),
+                new EmployeePayrollData(0, "Salman", 5000.0)
+        };
+        Instant threadStart = Instant.now();
+        addEmployeeToJsonServerWithThreads(Arrays.asList(arrayOfEmps));
+        Instant threadEnd = Instant.now();
+        System.out.println("Duration with thread" + Duration.between(threadStart, threadEnd));
+        Response response = RestAssured.get("/employees");
+        response.then().body("name", Matchers.hasItems("Ajit"));
+    }
+
+    private void addEmployeeToJsonServerWithThreads(List<EmployeePayrollData> asList)
+    {
+        HashMap<Integer, Boolean> empAdditionStatus = new HashMap<>();
+        asList.forEach(employeePayrollData -> {
+            Runnable task = () -> {
+                empAdditionStatus.put(employeePayrollData.hashCode(), false);
+                System.out.println("Employee being added: " + Thread.currentThread().getName());
+                String empDetails = "{\"name\": \"" + employeePayrollData.name + "\", \"salary\": \"" + employeePayrollData.salary + "\"}";
+                addEmpToJSON(empDetails);
+                empAdditionStatus.put(employeePayrollData.hashCode(), true);
+                System.out.println("Employee added: " + Thread.currentThread().getName());
+            };
+            Thread thread = new Thread(task, employeePayrollData.name);
+            thread.start();
+        });
+        while (empAdditionStatus.containsValue(false))
+        {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void addEmpToJSON(String employeePayrollData)
+    {
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(employeePayrollData)
+            .when()
+            .post("/employees");
     }
 }
